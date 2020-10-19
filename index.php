9e7491d32ada8d6db1d698cd3cc26f7f2d519f91
kishannk11 <35322004+kishannk11@users.noreply.github.com>
2020-10-20 00:43:25 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<?php include 'header.php';?>
+
+
+
+
+<!-- banner -->
+<div class="banner">    	   
+    <img src="images/photos/BG.jpg"  class="img-responsive" alt="slide">
+    <div class="welcome-message">
+        <div class="wrap-info">
+            <div class="information">
+                <h1  class="animated fadeInDown" >Sarva Resorts</h1>
+                <p class="animated fadeInUp">Find the best resort on your fingertips</p>                
+            </div>
+            <a href="#information" class="arrow-nav scroll wowload fadeInDownBig"><i class="fa fa-angle-down"></i></a>
+        </div>
+    </div>
+</div>
+<!-- banner-->
+
+
+
+
+
+
+
+
+<div class="container">
+
+<h2>Rooms & Tariff</h2>
+
+
+<!-- form -->
+
+<div class="row">
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/Eagle1.jpg" class="img-responsive"><div class="info"><h3>Eagle Homestay</h3><p>Contents Needed</p><a href="room-details-eagle.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/Naveen1.jpg" class="img-responsive"><div class="info"><h3>Naveen Homestay</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details-naveen.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/Nanjun1.jpg" class="img-responsive"><div class="info"><h3>Nanjundeshwara Homestay</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details-nanjun.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-5 wowload fadeInUp"><div class="rooms"><img src="images/photos/11.jpg" class="img-responsive"><div class="info"><h3>Homestay 4</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-5 wowload fadeInUp"><div class="rooms"><img src="images/photos/9.jpg" class="img-responsive"><div class="info"><h3>Homestay 5</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-5 wowload fadeInUp"><div class="rooms"><img src="images/photos/8.jpg" class="img-responsive"><div class="info"><h3>Homestay 6</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-5 wowload fadeInUp"><div class="rooms"><img src="images/photos/10.jpg" class="img-responsive"><div class="info"><h3>Homestay 7</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-5 wowload fadeInUp"><div class="rooms"><img src="images/photos/11.jpg" class="img-responsive"><div class="info"><h3>Homestay 8</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+</div>
+
+                     
+
+
+</div>
+
+<!-- reservation-information -->
+<div id="information" class="spacer reserve-info ">
+<div class="container">
+<div class="row">
+<div class="col-sm-7 col-md-8">
+    <div class="embed-responsive embed-responsive-16by9 wowload fadeInLeft"><iframe  src="https://www.youtube.com/watch?v=gBHWHcrTghk&ab_channel=sumanthholla" width="100%" height="400" frameborder="0"></iframe></div>
+</div>
+<div class="col-sm-5 col-md-4">
+<h3>Reservation</h3>
+    <form role="form" class="wowload fadeInRight">
+        <div class="form-group">
+            <input type="text" class="form-control"  placeholder="Name">
+        </div>
+        <div class="form-group">
+            <input type="email" class="form-control"  placeholder="Email">
+        </div>
+        <div class="form-group">
+            <input type="Phone" class="form-control"  placeholder="Phone">
+        </div>        
+        <div class="form-group">
+            <div class="row">
+            <div class="col-xs-6">
+            <select class="form-control">
+              <option>No. of Rooms</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+            </div>        
+            <div class="col-xs-6">
+            <select class="form-control">
+              <option>No. of Adult</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+            </div></div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+            <div class="col-xs-4">
+              <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
+                <option>Date</option>
+                <option value="01">1</option>
+                <option value="02">2</option>
+                <option value="03">Mar (03)</option>
+                <option value="04">Apr (04)</option>
+                <option value="05">May (05)</option>
+                <option value="06">June (06)</option>
+                <option value="07">July (07)</option>
+                <option value="08">Aug (08)</option>
+                <option value="09">Sep (09)</option>
+                <option value="10">Oct (10)</option>
+                <option value="11">Nov (11)</option>
+                <option value="12">Dec (12)</option>
+              </select>
+            </div>
+            <div class="col-xs-4">
+              <select class="form-control col-sm-2" name="expiry-month" id="expiry-month">
+                <option>Month</option>
+                <option value="01">Jan (01)</option>
+                <option value="02">Feb (02)</option>
+                <option value="03">Mar (03)</option>
+                <option value="04">Apr (04)</option>
+                <option value="05">May (05)</option>
+                <option value="06">June (06)</option>
+                <option value="07">July (07)</option>
+                <option value="08">Aug (08)</option>
+                <option value="09">Sep (09)</option>
+                <option value="10">Oct (10)</option>
+                <option value="11">Nov (11)</option>
+                <option value="12">Dec (12)</option>
+              </select>
+            </div>
+            <div class="col-xs-4">
+              <select class="form-control" name="expiry-year">
+                <option value="20">2020</option>
+                <option value="21">2021</option>
+                <option value="22">2022</option>
+                <option value="23">2023</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+            <textarea class="form-control"  placeholder="Message" rows="4"></textarea>
+        </div>
+        <button class="btn btn-default">Submit</button>
+    </form>    
+</div>
+</div>  
+</div>
+</div>
+<!-- reservation-information -->
+
+
+<?php include 'footer.php';?>
