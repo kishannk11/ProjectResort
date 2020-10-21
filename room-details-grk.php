0ca41bb84406b2eca7d6b168e0f1ce4caa61d84e
kishannk11 <35322004+kishannk11@users.noreply.github.com>
2020-10-22 02:15:47 +0530

    Create room-details-grk.php

---
+++ room-details-grk.php
@@ -0,0 +1,52 @@
+<?php include 'header.php';?>
+
+<div class="container">
+
+<h1 class="title">GRK Homestay</h1>
+
+
+
+ <!-- RoomDetails -->
+            <div id="RoomDetails" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                <div class="item active"><img src="images/photos/Eagle1.jpg" class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/Eagle2.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/Eagle3.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                <!-- Controls -->
+                <a class="left carousel-control" href="#RoomDetails" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                <a class="right carousel-control" href="#RoomDetails" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+  <!-- RoomCarousel-->
+
+<div class="room-features spacer">
+  <div class="row">
+    <div class="col-sm-12 col-md-5"> 
+    <p>Feel Like Home!This sentence You Should Take Literally When Staying Here.The Hosts Welcome and Care For You,If You Were a Good Friend That Comes For a Visit.They Prepare Something Enjoyable To Eat For You,Know Every Insider Tip Of The City And Always Have an Open Ear.Welcome To Your Friend'S Place!.... </p>
+    <p></p>
+    </div>
+    <div class="col-sm-6 col-md-3 amenitites"> 
+    <h3>Amenitites</h3>    
+    <ul>
+      <li>Hot Water</li>
+      <li>CCTV Surveilance</li>
+      <li>Wifi and TV</li>
+      <li>Parking</li>
+      <li></li>
+    </ul>
+    
+
+    </div>  
+    <div class="col-sm-3 col-md-2">
+      <div class="size-price">Size<span>44 sq</span></div>
+    </div>
+    <div class="col-sm-3 col-md-2">
+      <div class="size-price">Price<span>$200.00</span></div>
+    </div>
+  </div>
+</div>
+                     
+
+
+</div>
+<?php include 'footer.php';?>
