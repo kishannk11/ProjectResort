a4a2ff05602f260aa57880c6af389b7980d65644
rohan <rohan2000rohith@gmail.com>
2020-10-31 17:24:39 +0530

    Update room-details-dweepa.php

--- room-details-dweepa.php
+++ room-details-dweepa.php
@@ -9,9 +9,12 @@
  <!-- RoomDetails -->
             <div id="RoomDetails" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
-                <div class="item active"><img src="images/photos/Eagle1.jpg" class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/Eagle2.jpg"  class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/Eagle3.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item active"><img src="images/photos/dweepa/dweepa1.jpg" class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/dweepa/dweepa2.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/dweepa/dweepa3.jpg"  class="img-responsive" alt="slide"></div>
+				<div class="item  height-full"><img src="images/photos/dweepa/dweepa4.jpg"  class="img-responsive" alt="slide"></div>
+				<div class="item  height-full"><img src="images/photos/dweepa/dweepa5.jpg"  class="img-responsive" alt="slide"></div>
+				<div class="item  height-full"><img src="images/photos/dweepa/dweepa6.jpg"  class="img-responsive" alt="slide"></div>
                 </div>
                 <!-- Controls -->
                 <a class="left carousel-control" href="#RoomDetails" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
