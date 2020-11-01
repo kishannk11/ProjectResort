25ba4acd1504ece874cf5136bffea5585f6e7f1b
kishannk11 <35322004+kishannk11@users.noreply.github.com>
2020-11-02 02:49:51 +0530

    Update introduction.php

--- introduction.php
+++ introduction.php
@@ -1,16 +1,18 @@
 <?php include 'header.php';?>
 <div class="container">
 
-       <h1 class="title">Introduction</h1>
+       <h1 class="title">COORG</h1>
        <div class="row">
-              <div class="col-sm-4"><p>Chikmagalore is well</p></div>
-              <div class="col-sm-4"><p>Fale acid rest bock, " trappist biere de garde," infusion oxidized, carboy. hard cider, crystal malt hoppy pitch alpha acid dry stout tulip glass fermentation. bitter malt extract, dunkle, squares. units of bitterness trappist wort chiller keg krausen ester. brewpub. cold filter length, alcohol copper degrees plato aau.</p></div>
-              <div class="col-sm-4"><p>Dry stout aau pilsner. bock cold filter lauter tun specific gravity degrees plato ibu cold filter. brew kettle crystal malt, hard cider ipa bright beer caramel malt. pitching aerobic keg finishing hops pub units of bitterness.</p></div>              
+              <div class="col-sm-4"><p>Madikeri is a hill town in southern India. Madikeri was formerly known as Muddurajakeri. Muddurajakeri which means Mudduraja's town, was named after the prominent Haleri king, Mudduraja who ruled Kodagu from 1633-1687. 
+                                       Framed by the Western Ghats mountain range, it’s known for the Raja’s Seat, a simple monument </p></div>
+              <div class="col-sm-4"><p>overlooking forests and rice paddies. In the center, the 17th-century Madikeri Fort features 2 stone elephants at the entrance. 
+									   Nearby, the domed Omkareshwar Temple is dedicated to the Hindu deity Shiva. To the northwest, cascading Abbey Falls is surrounded by spice plantations.</p></div>
+              <div class="col-sm-4"><p>We welcome you to Coorg to have a great Holiday time with your Loved ones. Explore the city with a plesant vibes of the Nature. We provide you one of the best service and confortable accomodation facility.<br> &nbsp &nbsp &nbsp &nbsp <b> WELCOME TO COORG </b><br> &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp - Sarva Resorts Team</p></div>              
        </div>
 
 
        <div class="spacer">
-			<div class="embed-responsive embed-responsive-16by9 wowload fadeInLeft"><iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/AO-CfYKNBYE?autoplay=1&showinfo=0&controls=0&rel=0&showinfo=0&disablekb=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe></div>
+			<div class="embed-responsive embed-responsive-16by9 wowload fadeInLeft"><iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/gO3RDRpxumw?autoplay=1&showinfo=0&controls=0&rel=0&showinfo=0&disablekb=1&loop=1&modestbranding=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe></div>
        </div>
 
 
