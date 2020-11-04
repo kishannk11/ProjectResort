7c9a21ce4dde1b907f34419ae012add77346afab
kishannk11 <35322004+kishannk11@users.noreply.github.com>
2020-11-05 03:11:54 +0530

    Update room-details-grk.php

--- room-details-grk.php
+++ room-details-grk.php
@@ -23,26 +23,32 @@
 <div class="room-features spacer">
   <div class="row">
     <div class="col-sm-12 col-md-5"> 
-    <p>Feel Like Home!This sentence You Should Take Literally When Staying Here.The Hosts Welcome and Care For You,If You Were a Good Friend That Comes For a Visit.They Prepare Something Enjoyable To Eat For You,Know Every Insider Tip Of The City And Always Have an Open Ear.Welcome To Your Friend'S Place!.... </p>
-    <p></p>
-    </div>
+    <p>Feel Like Home! This sentence You Should Take Literally When Staying Here. The Hosts Welcome and Care For You, If You Were a Good Friend That Comes For a Visit.</p>
+    <p>GRK Resort is 28km away from Abbi falls and surrounded with GRK viewpoint hills and other places like Manzrabadh Fort, Bisle Ghat, Mallalli Falls, Railway Tunnel, Bettadha Baireshwara</p>
+    
+	<p>Address: <b>GRK Resort, Achanahalli, Sakleshpur</b></p>
+	
+	</div>
+	
+	
     <div class="col-sm-6 col-md-3 amenitites"> 
-    <h3>Amenitites</h3>    
+    <h3>Activities</h3>    
     <ul>
-      <li>Hot Water</li>
-      <li>CCTV Surveilance</li>
-      <li>Wifi and TV</li>
-      <li>Parking</li>
-      <li></li>
+      <li>Indoor Games</li>
+      <li>Firecamp</li>
+      <li>Mud Vollyball</li>
+      <li>Swimming</li>
+      <li>Jeep Safari</li>
+	  *(Jeep Safari costs extra charges) 
     </ul>
     
 
     </div>  
     <div class="col-sm-3 col-md-2">
-      <div class="size-price">Size<span>44 sq</span></div>
+      <div class="size-price">Price<span>₹1800.00</span>(Tent Stay)</div>
     </div>
     <div class="col-sm-3 col-md-2">
-      <div class="size-price">Price<span>$200.00</span></div>
+      <div class="size-price">Price<span>₹2700.00</span>(with Activities)</div>
     </div>
   </div>
 </div>
