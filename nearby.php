057fc7bc8bac5155882a4701851a6559befc19a5
kishannk11 <35322004+kishannk11@users.noreply.github.com>
2020-11-05 03:37:43 +0530

    Update nearby.php

--- nearby.php
+++ nearby.php
@@ -22,15 +22,22 @@
 				</a>
 			</div>
 			
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/4.jpg" title="Adventure" class="gallery-image" data-gallery><img src="images/photos/4.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/5.jpg" title="Fruits" class="gallery-image" data-gallery><img src="images/photos/5.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/6.jpg" title="Summer" class="gallery-image" data-gallery><img src="images/photos/6.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/7.jpg" title="Bathroom" class="gallery-image" data-gallery><img src="images/photos/7.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/8.jpg" title="Rooms" class="gallery-image" data-gallery><img src="images/photos/8.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/9.jpg" title="Big Room" class="gallery-image" data-gallery><img src="images/photos/9.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/11.jpg" title="Living Room" class="gallery-image" data-gallery><img src="images/photos/11.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/1.jpg" title="Fruits" class="gallery-image" data-gallery><img src="images/photos/1.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/3.jpg" title="Travel" class="gallery-image" data-gallery><img src="images/photos/3.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/1.jpg" title="Baba Budangiri" class="gallery-image" data-gallery><img src="images/photos/4.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/2.jpg" title="Abbey Falls" class="gallery-image" data-gallery><img src="images/photos/5.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/3.jpg" title="Kayaking" class="gallery-image" data-gallery><img src="images/photos/6.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/4.jpg" title="Sunset" class="gallery-image" data-gallery><img src="images/photos/7.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/5.jpg" title="Agumbe" class="gallery-image" data-gallery><img src="images/photos/8.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/6.jpg" title="Jog Falls" class="gallery-image" data-gallery><img src="images/photos/9.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/7.jpg" title="Tea Estate" class="gallery-image" data-gallery><img src="images/photos/11.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/8.jpg" title="Tea Estate" class="gallery-image" data-gallery><img src="images/photos/1.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/9.jpg" title="Church" class="gallery-image" data-gallery><img src="images/photos/3.jpg" class="img-responsive"></a></div>
+			<div class="col-sm-4 wowload fadeInUp"><a href="images/photos/10.jpg" title="Mangalore" class="gallery-image" data-gallery><img src="images/photos/4.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/11.jpg" title="Sakaleshpura" class="gallery-image" data-gallery><img src="images/photos/5.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/12.jpg" title="Dubare" class="gallery-image" data-gallery><img src="images/photos/6.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/13.jpg" title="Coorg" class="gallery-image" data-gallery><img src="images/photos/7.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/14.jpg" title="Rooms" class="gallery-image" data-gallery><img src="images/photos/8.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/15.jpg" title="Big Room" class="gallery-image" data-gallery><img src="images/photos/9.jpg" class="img-responsive"></a></div>
+            
        </div>
 </div>
 <?php include 'footer.php';?>
