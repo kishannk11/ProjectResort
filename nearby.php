924e4395af341e587e0b099dc04b6473d0674606
Ranjith <ranjithchandran220@gmail.com>
2020-11-16 11:15:28 +0530

    Update nearby.php

--- nearby.php
+++ nearby.php
@@ -27,7 +27,7 @@
             <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/4.jpg" title="Sunset" class="gallery-image" data-gallery><img src="images/photos/7.jpg" class="img-responsive"></a></div>
             <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/5.jpg" title="Agumbe" class="gallery-image" data-gallery><img src="images/photos/8.jpg" class="img-responsive"></a></div>
             <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/6.jpg" title="Jog Falls" class="gallery-image" data-gallery><img src="images/photos/9.jpg" class="img-responsive"></a></div>
-            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/7.jpg" title="Tea Estate" class="gallery-image" data-gallery><img src="images/photos/11.jpg" class="img-responsive"></a></div>
+            <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/7.jpg" title="Tea Estate" class="gallery-image" data-gallery><img src="images/photos/7.jpg" class="img-responsive"></a></div>
             <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/8.jpg" title="Tea Estate" class="gallery-image" data-gallery><img src="images/photos/1.jpg" class="img-responsive"></a></div>
             <div class="col-sm-4 wowload fadeInUp"><a href="images/photos/9.jpg" title="Church" class="gallery-image" data-gallery><img src="images/photos/3.jpg" class="img-responsive"></a></div>
 			<div class="col-sm-4 wowload fadeInUp"><a href="images/photos/10.jpg" title="Mangalore" class="gallery-image" data-gallery><img src="images/photos/4.jpg" class="img-responsive"></a></div>
