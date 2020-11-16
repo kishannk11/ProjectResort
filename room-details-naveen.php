1656041ac1509da4b48d20dba4245b575daf3237
Ranjith <ranjithchandran220@gmail.com>
2020-11-16 10:52:30 +0530

    Update room-details-naveen.php

--- room-details-naveen.php
+++ room-details-naveen.php
@@ -9,11 +9,11 @@
  <!-- RoomDetails -->
             <div id="RoomDetails" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
-                <div class="item active"><img src="images/photos/naveen/Naveen1.jpg" class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/naveen/Naveen2.jpg"  class="img-responsive" alt="slide"></div>
-                <div class="item  height-full"><img src="images/photos/naveen/Naveen3.jpg"  class="img-responsive" alt="slide"></div>
-				<div class="item  height-full"><img src="images/photos/naveen/Naveen4.jpg"  class="img-responsive" alt="slide"></div>
-				<div class="item  height-full"><img src="images/photos/naveen/Naveen5.jpg"  class="img-responsive" alt="slide"></div>
+                <div class="item active"><img src="images/photos/naveen/Naveen1.JPG" class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/naveen/Naveen2.JPG"  class="img-responsive" alt="slide"></div>
+                <div class="item  height-full"><img src="images/photos/naveen/Naveen3.JPG"  class="img-responsive" alt="slide"></div>
+				<div class="item  height-full"><img src="images/photos/naveen/Naveen4.JPG"  class="img-responsive" alt="slide"></div>
+				<div class="item  height-full"><img src="images/photos/naveen/Naveen5.JPG"  class="img-responsive" alt="slide"></div>
                 </div>
                 <!-- Controls -->
                 <a class="left carousel-control" href="#RoomDetails" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
